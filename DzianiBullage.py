09fa8a7031c3bf266ec5d5eb96d29f6cd6f7b5cf
bonaime <bonaime@ipgp.fr>
2025-06-24 14:44:44 +0200

    full

--- DzianiBullage.py
+++ DzianiBullage.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import argparse
 import itertools
+from math import dist
 import os
 import sys
 import csv
 import socket
 from pathlib import Path
-from multiprocessing import Pool, cpu_count
+from multiprocessing import Pool, RLock, cpu_count
 from dataclasses import dataclass, field
 import datetime
 import psutil
@@ -17,10 +18,11 @@ from matplotlib import colors
 from scipy.interpolate import griddata,interp1d, UnivariateSpline
 from scipy import ndimage
 from dotenv import load_dotenv
-from tqdm import trange
+from tqdm import tqdm, trange
 from tqdm.contrib.concurrent import process_map
 from codetiming import Timer
 from utils import get_data_from_google_sheet
+from tslearn.barycenters import euclidean_barycenter
 import gc
 
 @dataclass
@@ -336,7 +338,7 @@ class DzianiBullage:
 
         video_file = cv2.VideoCapture(self.video_path)
         if not video_file.isOpened():
-            print(f"Error while opening video file {self.video_path}")
+            print(f"341 : Error while opening video file {self.video_path}")
             sys.exit()
 
         frames_per_window = int(self.window_size_seconds * self.frames_per_second)
@@ -781,6 +783,345 @@ class DzianiBullage:
 
         #return grid_X, self.grid_Y, self.smoothed_grid_speeds
 
+    def findIntersection(self,L1,L2):
+        x=(L1[1]-L2[1])/(L2[0]-L1[0])
+        y=L1[0]*x+L1[1]
+        return(x,y)
+
+
+    def calcul_centre(self,seuils_classes_distances,SECONDS_TO_COMPUTE,decalage,nb_total_decalage,numero_decalage):
+        NB_POINTS: int = 5000
+        rayon_cercle_largeur_ligne = 1
+
+        #print(f'{SECONDS_TO_COMPUTE=}\t{decalage=}\t{nb_total_decalage=}\t{numero_decalage=}')
+
+        #Définition des résultats des calculs
+        distances_totales = {}  # Distances totales parcourues par chaque point
+        total_times = {}  # Temps total de suivi pour chaque point
+        speed_m_per_sec_par_trajet = {} # Dictionnaire où chaque trajet correspond à une liste qui contient les vitesses prises par chaque point du trajet en m/s
+        all_points = [] # Liste pour stocker tous les points de trajectoire
+        speeds_m_per_sec = [] # Liste pour stocker les vitesses en m/s pour chaque point
+        trajets =  {}
+
+        video_file = cv2.VideoCapture(self.video_path)
+        if not video_file.isOpened():
+            print(f"810 : Error while opening video file {self.video_path}")
+            sys.exit()
+
+        # Obtenir le nombre total de frames
+        detection_center = self.frame_width // 2, self.frame_height // 2
+        detection_diameter = int(min(self.frame_width /2, self.frame_height/2))
+        #print(f'{detection_diameter=}\n{detection_center=}')
+
+        # Obtenir le taux de frames par seconde (fps)
+        #frames_per_second = round(video_file.get(cv2.CAP_PROP_FPS))
+        FRAMES_TO_COMPUTE  = SECONDS_TO_COMPUTE * self.frames_per_second
+
+
+        # Masques pour les zones de détection
+        masque_detection = np.zeros((self.frame_height,self.frame_width), dtype=np.uint8)  # Crée un masque de la même taille que l'image, mais en niveaux de gris
+        # Dessine un cercle plein (rayon 500) sur le masque avec une valeur de 255 (blanc)
+        cv2.circle(masque_detection, detection_center, detection_diameter, 255, thickness=-1)
+
+        # Decalage du film pour se mettre au bon endroit pour les calculs
+        video_file.set(cv2.CAP_PROP_POS_FRAMES, decalage * self.frames_per_second)
+        frame_available, frame = video_file.read()
+
+
+        previous_frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #image_precedente_grise = frame_to_grey_sum(image_precedente)
+
+        # Utilise le masque full pour tout détecter
+        positions_initiales = cv2.goodFeaturesToTrack(previous_frame_gray,mask=masque_detection, **self.DETECTION_PARAMETERS)
+
+
+        #nb_shift_total =int(movie_length_seconds - window_size_seconds/windows_shift_seconds)
+        table_colors = plt.colormaps.get_cmap('plasma').resampled(nb_total_decalage).colors
+
+        # Boucle de traitement pour chaque frame jusqu'à atteindre frames_per_window
+        t = trange(FRAMES_TO_COMPUTE, desc=f'{decalage:03} frame analysis ',
+                position=numero_decalage,        leave=True,                # the progress bar will be cleared up and the cursor position unchanged when finished
+
+                colour=colors.rgb2hex(table_colors[numero_decalage]))
+
+    #    for frame_count in range(FRAMES_TO_COMPUTE):
+    #        print(f'{decalage:03}  {frame_count:03})')
+        for frame_count in t:
+            #t.set_postfix(refresh=False)
+
+            #avant_read_frame = time.time()
+            frame_available, frame = video_file.read()
+
+            # arret si pb de lecture ==fin de fichier
+            if not frame_available:
+                break
+
+            #Calcul du flux optique pour suivre les caractéristiques d'une frame à l'autre
+            frame_gray = self.frame_to_BGR2GRAY(frame)
+            masque_suivi = np.zeros_like(frame)
+
+            positions_suivies, statuts,err = cv2.calcOpticalFlowPyrLK(previous_frame_gray, frame_gray, positions_initiales, None, **self.TRACKING_PARAMETERS)
+
+            # Filtrer les bons points suivis dans la nouvelle frame
+            points_encore_suivis = positions_suivies[statuts == 1]
+            points_initiaux_encore_suivis = positions_initiales[statuts == 1]
+
+            previous_frame_gray = frame_gray.copy()
+            positions_initiales = points_encore_suivis.reshape(-1, 1, 2)
+
+
+            if len(masque_suivi.shape) == 2 or masque_suivi.shape[2] == 1:  # mask est en niveaux de gris
+                masque_suivi = cv2.cvtColor(masque_detection, cv2.COLOR_GRAY2BGR)
+
+
+            for i, (new, old) in enumerate(zip(points_encore_suivis, points_initiaux_encore_suivis)):
+
+                x_new_point, y_new_point = new.ravel()
+                x_old_point, y_old_point = old.ravel()
+
+                distance = np.sqrt((x_new_point - x_old_point) ** 2 + (y_new_point - y_old_point) ** 2)
+
+                all_points.append(new)  # Stocker le point
+
+                # Initialisation s'ils n'existent pas déjà
+                if i not in distances_totales:
+                    distances_totales[i] = 0
+
+                # Initialisation s'ils n'existent pas déjà
+                if i not in trajets:
+                    trajets[i] = []
+                trajets[i].append(new.ravel())
+
+                # Mise à jour des distances pour chaque point suivi
+                distances_totales[i] += distance
+
+                    #https://excelatfinance.com/xlf/xlf-colors-1.php
+
+        video_file.release()
+
+
+
+        ### Distribution des distances en histogamme
+        if False :
+            array_distance =[]
+            for key in distances_totales.keys():
+                #print(key)
+                array_distance.append(distances_totales[key])
+
+            cm = plt.colormaps.get_cmap('plasma')
+
+            n, bins, patches = plt.hist(array_distance, color='lightgreen', ec='black', bins=100)
+            bin_centers = 0.5 * (bins[:-1] + bins[1:])
+
+            # scale values to interval [0,1]
+            col = bin_centers - min(bin_centers)
+            col /= max(col)
+
+            for c, p in zip(col, patches):
+                plt.setp(p, 'facecolor', cm(c))
+
+            #plt.show()
+
+        AFFICHAGE_TRAJETS = False
+
+        masque_suivi = np.zeros_like(frame)
+        trajets_longs={}
+        for key, value in distances_totales.items():
+            if value < seuils_classes_distances[0]:
+                #Cyan
+                color = (0,255,255)
+                if AFFICHAGE_TRAJETS :
+                    for point in trajets[key] :
+                        x_new_point, y_new_point = point.ravel()
+                        cv2.circle(masque_suivi, (int(x_new_point), int(y_new_point)), rayon_cercle_largeur_ligne, color , -1)
+
+            if distances_totales[key] >  seuils_classes_distances[0]  and distances_totales[key] <  seuils_classes_distances[1]:
+                # Fushia
+                color = (255,0,255)
+                if AFFICHAGE_TRAJETS :
+                    for point in trajets[key] :
+                        x_new_point, y_new_point = point.ravel()
+                        #cv2.circle(masque_suivi, (int(x_new_point), int(y_new_point)), rayon_cercle_largeur_ligne, color , -1)
+
+            elif distances_totales[key] >  seuils_classes_distances[1]:
+                trajets_longs[key]=np.array(trajets[key].copy())
+                # Rouge
+                color = (0,0,255)
+
+                if AFFICHAGE_TRAJETS :
+                    for point in trajets[key] :
+                        x_new_point, y_new_point = point.ravel()
+                        cv2.circle(masque_suivi, (int(x_new_point), int(y_new_point)), rayon_cercle_largeur_ligne, color , -1)
+
+        # Dessine le cercle de détection sur le masque avec une valeur de 255 (blanc)
+        #cv2.circle(masque_suivi, detection_center, detection_diameter, (255,255,255), thickness=20)
+
+        if AFFICHAGE_TRAJETS:
+            img = cv2.add(frame, masque_suivi)
+            cv2.imshow('frame',img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            #save_trajet(img, "vitesse.png")
+
+
+        interpolations = {}
+        trajets_coefs_all =[]
+        i =0
+        #print(f'{decalage:03}  interpolation des trajets long')
+
+        for trajet in trajets_longs.values():
+            trajet = np.array(trajet)
+            min_trajet_x, max_trajet_x = min(trajet[:,0]), max(trajet[:,0])
+            # Create an evenly spaced array that ranges from the minimum to the maximum
+            # This will be used as your new x coordinates
+            new_trajets_x = np.linspace(min_trajet_x, max_trajet_x, 100)
+
+            # Fit a 1rd degree polynomial to your data
+            trajets_coefs = np.polyfit(trajet[:,0],trajet[:,1], 1)
+            trajets_coefs_all.append(trajets_coefs)
+            interpolation_trajet_x = np.linspace(min_trajet_x, max_trajet_x, 100)
+            interpolation_trajet_y = np.polyval(trajets_coefs, interpolation_trajet_x)
+            interpolations[i] =[interpolation_trajet_x,interpolation_trajet_y]
+
+            i=i+1
+
+        # find intersections
+
+        # On garde un nb pair de solutions
+        if len(trajets_coefs_all) %2 !=0:
+            trajets_coefs_all=trajets_coefs_all[:-1]
+
+        solutions=[]
+        # random de toutes les interpolations
+        np.random.shuffle(trajets_coefs_all)
+        #print(f'{decalage:03}  calculs solutions')
+        for couple in [(trajets_coefs_all[i],trajets_coefs_all[i+1]) for i in range(0,len(trajets_coefs_all),2)]:
+            a =couple[0]
+            b = couple[1]
+            solutions.append(self.findIntersection(a,b))
+
+
+
+
+        #plt.imshow(frame)
+
+        # plot interpolations
+        #for inter in interpolations:
+        #    interpolation_trajet_x = interpolations[inter][0]
+        #    interpolation_trajet_y = interpolations[inter][1]
+        #    plt.plot(interpolation_trajet_x, interpolation_trajet_y,linewidth=0.5,color=(random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)))
+
+
+        #print(f'{decalage:03}  filtrage solutions')
+        filtered_solutions=[]
+        for solution in solutions:
+            x= solution[0]
+            y = solution[1]
+
+            # la solution doit etre dans l'image
+            # et  pas etre trop loin du center
+            if x>0 and x< self.frame_width and y > 0 and y < self.frame_height and dist(solution,detection_center)<1500:
+                filtered_solutions.append(solution)
+                #plt.plot(x,y,color='red', marker='o', linestyle='dashed',
+                #linewidth=2, markersize=3)
+        #print(f'{decalage:03} nb de solutions {len(filtered_solutions)}')
+
+        #print(f'{decalage:03} calcul barycentre')
+        return euclidean_barycenter(filtered_solutions)
+
+
+
+    def calcul_centre_panache(self):
+        SHOW_IMAGES = False
+        SAVE_PLOTS = True
+
+
+        # if numero >2:
+        #     return
+
+        print(f'Computing center for{ self.input_video_filename}')
+
+        # Vérifier si le fichier existe
+        if not os.path.isfile(self.video_path):
+            print(f"Erreur: Le fichier {self.video_path} n'existe pas.")
+            sys.exit()
+
+        liste_centres=[]
+
+        video_file = cv2.VideoCapture(self.video_path)
+        if not video_file.isOpened():
+            print('#################################################################################################')
+            print(f"1054 : Error while opening video file {self.video_path}")
+            print('#################################################################################################')
+            return
+        frame_available, background_frame = video_file.read()
+
+        # Obtenir le nombre total de frames
+        total_frames = int(video_file.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(video_file.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(video_file.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        frames_per_second = round(video_file.get(cv2.CAP_PROP_FPS))
+        movie_length_seconds = round(total_frames / frames_per_second)
+        frame_time = 1 / frames_per_second  # Durée d'un frame en secondes
+        detection_center = frame_width // 2, frame_height // 2
+
+        video_file.release()
+
+        seuils_classes_distances = [70 , 90] # ok pour 5,5
+    #    seuils_classes_distances = [90 , 100] # ok pour 5,5
+        window_size_seconds = 5
+
+        windows_shift_seconds = 5
+        movie_length_seconds = min(movie_length_seconds, 100)
+
+        cpu_nb = len(psutil.Process().cpu_affinity())
+        decalage_list = list(range(0, movie_length_seconds - window_size_seconds, windows_shift_seconds))
+        array_arguments_for_calcul_centre =  [(seuils_classes_distances, window_size_seconds, decalage,len(decalage_list), index ) for index, decalage in enumerate(decalage_list)]
+
+        cpu_nb = min(cpu_nb, len(decalage_list))
+        print(f'{cpu_nb=}')
+    #    print(array_arguments_for_calcul_centre)
+
+        with Pool(processes=cpu_nb,initargs=(RLock(),), initializer=tqdm.set_lock) as pool:
+            # Utiliser pool.map pour appliquer la fonction calculer_vitesse_bulles à chaque élément
+            #  de la array_arguments_for_calculer_vitesse_bulles
+            barycenters=pool.starmap(self.calcul_centre, array_arguments_for_calcul_centre)
+
+
+
+        filtered_centers=[]
+        plt.imshow(background_frame)
+        for center in barycenters:
+            x,y = center[0],center[1]
+            #print( math.dist(center,detection_center))
+            #if x>0 and x< frame_width and y > 0 and y < frame_height and math.dist(center,detection_center)<800:
+            if x>0 and x< frame_width and y > 0 and y < frame_height :
+                filtered_centers.append(center)
+                plt.plot(x,y,color='blue', marker='o', linewidth=2, markersize=1)
+
+        ultimate_center =euclidean_barycenter(filtered_centers)
+
+        print(f'ultimate_center for {self.date_video} {self.video_path} {ultimate_center[0]},{ultimate_center[1]}')
+        if SHOW_IMAGES or SAVE_PLOTS:
+            plt.plot(ultimate_center[0],ultimate_center[1],color='red', marker='o', linewidth=2, markersize=3)
+        #plt.plot(centre_sarah[0],centre_sarah[1],color='yellow', marker='o', linewidth=2, markersize=7)
+        plt.tight_layout()
+
+        if SHOW_IMAGES :
+            plt.show()
+        if SAVE_PLOTS:
+            center_fig_name=self.output_path / f'{self.tag_file}_center.png'
+
+            plt.savefig(center_fig_name,dpi=150)
+        plt.close()
+
+
+        final_center = (int(ultimate_center[0][0]),int(ultimate_center[1][0]))
+        print(f'{final_center=}')
+        print(f'{self.tag_file}\tcentre zone de detetion {int(final_center[0])}, {int(final_center[1])}')
+
+
+
     def calcul_largeur_panache(self):
         resolution = 200
         # Dimensions
@@ -952,19 +1293,18 @@ def main():
 
     # Ajout de l'argument numero_ligne
     parser.add_argument('numero_ligne', type=int, help='Le numéro de ligne à traiter qui doit être un entier.')
+
+    parser.add_argument('--find_center','-c',default=False, action='store_true', help='flag pour trouver le centre du panache')
     parser.add_argument('--file_analysis','-a',default=False, action='store_true', help='flag pour faire l\'analyse du fichier')
+    parser.add_argument('--interpolation','-i',default=False, action='store_true', help='flag pour faire l\'interpolation')
     parser.add_argument('--largeur_panache','-l',default=False, action='store_true', help='flag pour calculer la largeur du panache')
 
     # Parsing des arguments
     args = parser.parse_args()
 
-    #numeros_des_lignes_a_traiter = [11]
     numero_ligne_a_traiter = args.numero_ligne
 
-
     print(datetime.datetime.now())
-
-
 
     duree_fenetre_analyse_seconde = 20
 
@@ -1025,17 +1365,18 @@ def main():
     # On calcul le gsd
     dziani_bullage.get_gsd()
 
+    if args.find_center :
+        with Timer(text="{name}: {:.4f} seconds", name="=> calcul centre panache"):
+                dziani_bullage.calcul_centre_panache()
+        sys.exit()
+
     if args.largeur_panache:
 
         with Timer(text="{name}: {:.4f} seconds", name="=> calcul largeur panache"):
             dziani_bullage.calcul_largeur_panache()
-        #### On ne refait pas les calculs !!!
         sys.exit()
 
     if args.file_analysis :
-
-        # modifier la longueur d'analyse du fichier.
-        #dziani_bullage.movie_length_seconds = 300
 
         with Timer(text="{name}: {:.4f} seconds", name="=> video_file_analysis"):
             dziani_bullage.video_file_analysis()
@@ -1048,30 +1389,38 @@ def main():
         sys.exit()
 
 
-    print("############################")
-    print("##### INTERPOLATION #######")
-    print("############################")
+    if args.interpolation :
 
-    # with Timer(text="{name}: {:.4f} seconds", name="=> load_results_cPickle"):
-    #     dziani_bullage.load_results_pickle()
+        print("############################")
+        print("##### INTERPOLATION #######")
+        print("############################")
 
-    print(datetime.datetime.now())
+        # with Timer(text="{name}: {:.4f} seconds", name="=> load_results_cPickle"):
+        #     dziani_bullage.load_results_pickle()
 
-    # Si l'analyse n'a pas été faite lors du lancement du script
-    # il faut charger les résultats
-    if not args.file_analysis :
+        print(datetime.datetime.now())
+
+        # Si l'analyse n'a pas été faite lors du lancement du script
+        # il faut charger les résultats
         with Timer(text="{name}: {:.4f} seconds", name="=> load_results_numpy"):
             dziani_bullage.load_results_numpy()
 
-    print(datetime.datetime.now())
+        print(datetime.datetime.now())
 
-    with Timer(text="{name}: {:.4f} seconds", name="=> interpolation"):
-        dziani_bullage.interpolation()
+        with Timer(text="{name}: {:.4f} seconds", name="=> interpolation"):
+            dziani_bullage.interpolation()
 
-    with Timer(text="{name}: {:.4f} seconds", name="=> save_results_numpy"):
-        dziani_bullage.save_results_interpolation_numpy()
+        with Timer(text="{name}: {:.4f} seconds", name="=> save_results_numpy"):
+            dziani_bullage.save_results_interpolation_numpy()
 
-    print(datetime.datetime.now())
+        print(datetime.datetime.now())
+        sys.exit()
+
+    if args.largeur_panache:
+
+        with Timer(text="{name}: {:.4f} seconds", name="=> calcul largeur panache"):
+            dziani_bullage.calcul_largeur_panache()
+        sys.exit()
 
 
 if __name__ == "__main__":
